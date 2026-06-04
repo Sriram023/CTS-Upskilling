6da7e1c26f0093e162f77f79bc6c53ad2d56a27c
Sriram Ravichandran <sriramravichandran2006@gmail.com>
2026-06-04 09:43:23 +0530

    Refactor EvenOddChecker

--- MODULE-3/Java/Exercise03/EvenOddChecker.java
+++ MODULE-3/Java/Exercise03/EvenOddChecker.java
@@ -7,12 +7,12 @@ public class EvenOddChecker {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a Number: ");
-        int number = sc.nextInt();
+        int a = sc.nextInt();
 
-        if (number % 2 == 0) {
-            System.out.println(number + " is an Even Number.");
+        if ( a % 2 == 0) {
+            System.out.println(a + " is an Even Number.");
         } else {
-            System.out.println(number + " is an Odd Number.");
+            System.out.println(a + " is an Odd Number.");
         }
 
         sc.close();
