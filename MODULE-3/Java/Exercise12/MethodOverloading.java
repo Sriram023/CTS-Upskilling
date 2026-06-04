172d2bc2b0b150a7396e7ada262194d369debfd4
Sriram Ravichandran <sriramravichandran2006@gmail.com>
2026-06-04 11:48:56 +0530

    second commit

--- MODULE-3/Java/Exercise12/MethodOverloading.java
+++ MODULE-3/Java/Exercise12/MethodOverloading.java
@@ -1,32 +1,14 @@
-/**
- * Exercise12 - MethodOverloading
- * 
- * Objective: Understand method overloading in Java.
- * 
- * Method overloading is a feature that allows a class to have more than one method
- * with the same name, as long as their parameter lists are different (differing in
- * number, type, or order of parameters). It is a form of compile-time polymorphism.
- */
 public class MethodOverloading {
-
-    // 1. Overloaded method to add two integers
-    public static int add(int a, int b) {
-        return a + b;
+    public static int add(int s, int r) {
+        return s + r;
     }
-
-    // 2. Overloaded method to add two doubles
-    public static double add(double a, double b) {
-        return a + b;
+    public static double add(double s, double r) {
+        return s + r;
     }
-
-    // 3. Overloaded method to add three integers
-    public static int add(int a, int b, int c) {
-        return a + b + c;
+    public static int add(int s, int r, int i) {
+        return s + r + i;
     }
-
     public static void main(String[] args) {
-        
-        // Calling and displaying results from each overloaded method
         System.out.println("Sum of Two Integers      : " + add(10, 20));
         System.out.println("Sum of Two Doubles       : " + add(12.3, 13.2));
         System.out.println("Sum of Three Integers    : " + add(10, 20, 30));
