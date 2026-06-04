1f1a256d8d59b84ec769e3498dde45478a7e84b3
Sriram Ravichandran <sriramravichandran2006@gmail.com>
2026-06-04 09:47:09 +0530

    Refactor LeapYearChecker

--- MODULE-3/Java/Exercise04/LeapYearChecker.java
+++ MODULE-3/Java/Exercise04/LeapYearChecker.java
@@ -7,12 +7,12 @@ public class LeapYearChecker {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a Year: ");
-        int year = sc.nextInt();
+        int year_custom = sc.nextInt();
 
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + " is a Leap Year.");
+        if ((year_custom % 4 == 0 && year_custom % 100 != 0) || (year_custom % 400 == 0)) {
+            System.out.println(year_custom + " is a Leap Year.");
         } else {
-            System.out.println(year + " is Not a Leap Year.");
+            System.out.println(year_custom + " is Not a Leap Year.");
         }
 
         sc.close();
